f5e3e5694e8a4358632d39ead70260909c54ff21
Hajarhjr <105655506+Hajarhjr@users.noreply.github.com>
2024-02-07 14:30:31 +0100

    Modifier la class library et Ajouter le Menu
    Creree une method 'addStudent' et 'addBook'

--- Library.java
+++ Library.java
@@ -1,7 +1,29 @@
 import java.util.ArrayList;
 public class Library {
-    public  ArrayList<String> book =new ArrayList<>();
-    public  ArrayList<String> student =new ArrayList<>();
+    public  ArrayList<Book> books =new ArrayList<>();
+    public  ArrayList<Student> students =new ArrayList<>();
 
+    public void addBook(Book newbook) {
+        if (books.contains(newbook)) {
+            System.out.println("Ce livre est deja dans la bibliotheque.");
+        } else {
+            books.add(newbook);
+            System.out.println(("livre ajouter avec sucsee !!!!"));
+        }
+    }
+    public void addStudent(Student newstudent){
+       if(students.contains(newstudent)){
+           System.out.println("ce etudiant est deja exist ");
+       }else{
+           students.add(newstudent);
+           System.out.println(("etudiant ajouter avec sucsee !!!"));
+       }
+    }
+     public void delete(Book newbook){
+          while(){
+
+          }
+        books.remove(newbook);
+     }
 
 }
--- Menu.java
+++ Menu.java
@@ -1,10 +1,67 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
-import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Menu {
 
     public static void main(String[] args) {
+        Library  library=new Library();
+        Scanner scanner =new Scanner(System.in);
+        int choix;
+do {
+    System.out.println("le menu:");
+    System.out.println("1. Ajouter un livre");
+    System.out.println("2. Ajouter un etudiant");
+    System.out.println("3. Quitter");
+    System.out.print("Entrez votre choix ");
+    choix = scanner.nextInt();
+    scanner.nextLine();
+    switch (choix) {
+        case 1:
+            Book newBook = getBook();
+            library.addBook(newBook);
+            break;
+        case 2:
+            Student newstudent = getStudent();
+            library.addStudent(newstudent);
+            break;
+        case 3:
+             System.out.println("au revoir !!");
+             break;
+        default:
+            System.out.println("erreur !!!!");
+            break;
+    }
+}while (choix!=3);
 
+    }
+
+    private static Student getStudent() {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("entrer le nom de l'Apprenant");
+        String nom = scanner.nextLine();
+        System.out.println("donner le l'adress de l'Apprenant ");
+        String adresse = scanner.nextLine();
+        System.out.println("donner le ID de L'Apprenant ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        Student newstudent = new Student(nom, adresse, id);
+        return newstudent;
+    }
+
+    private static Book getBook() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("entrez le titre du livre: ");
+        String titre = scanner.nextLine();
+        System.out.print("entrez l'auteur du livre: ");
+        String auteur = scanner.nextLine();
+        System.out.print("entrez le numero du livre: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("entrez la date de publication du livre: ");
+        String datepublication = scanner.nextLine();
+        Book newBook = new Book(titre, auteur, numero, datepublication);
+        return newBook;
     }
 }
